e7c8456170fc0037ff584cf6901e4289fe104bc5
olesgedz <olesgedz@rambler.ru>
2019-04-11 15:06:47 +0300

    map

--- srcs/main.c
+++ srcs/main.c
@@ -53,39 +53,42 @@ static struct player
 
 static void LoadData()
 {
-    FILE* fp = fopen("map-clear.txt", "rt");
-    if(!fp) { perror("map-clear.txt"); exit(1); }
+    FILE* fp = fopen("map2.txt", "rt");
+    if(!fp) { perror("map2.txt"); exit(1); }
     char Buf[256], word[256], *ptr;
     struct xy* vert = NULL, v;
     int n, m, NumVertices = 0;
-    while(fgets(Buf, sizeof Buf, fp))
-        switch(sscanf(ptr = Buf, "%32s%n", word, &n) == 1 ? word[0] : '\0')
-        {
-            case 'v': // vertex
-                for(sscanf(ptr += n, "%f%n", &v.y, &n); sscanf(ptr += n, "%f%n", &v.x, &n) == 1; )
-                    { vert = realloc(vert, ++NumVertices * sizeof(*vert)); vert[NumVertices-1] = v; }
-                break;
-            case 's': // sector
-                sectors = realloc(sectors, ++NumSectors * sizeof(*sectors));
-                struct sector* sect = &sectors[NumSectors-1];
-                int* num = NULL;
-                sscanf(ptr += n, "%f%f%n", &sect->floor,&sect->ceil, &n);
-                for(m=0; sscanf(ptr += n, "%32s%n", word, &n) == 1 && word[0] != '#'; )
-                    { num = realloc(num, ++m * sizeof(*num)); num[m-1] = word[0]=='x' ? -1 : atoi(word); }
-                sect->npoints   = m /= 2;
-                sect->neighbors = malloc( (m  ) * sizeof(*sect->neighbors) );
-                sect->vertex    = malloc( (m+1) * sizeof(*sect->vertex)    );
-                for(n=0; n<m; ++n) sect->neighbors[n] = num[m + n];
-                for(n=0; n<m; ++n) sect->vertex[n+1]  = vert[num[n]]; // TODO: Range checking
-                sect->vertex[0] = sect->vertex[m]; // Ensure the vertexes form a loop
-                free(num);
-                break;
-            case 'p':; // player
-                float angle;
-                sscanf(ptr += n, "%f %f %f %d", &v.x, &v.y, &angle,&n);
-                player = (struct player) { {v.x, v.y, 0}, {0,0,0}, angle,0,0,0, n }; // TODO: Range checking
-                player.where.z = sectors[player.sector].floor + EyeHeight;
-        }
+	while(fgets(Buf, sizeof Buf, fp))
+	{
+		switch(sscanf(ptr = Buf, "%32s%n", word, &n) == 1 ? word[0] : '\0')
+		{
+
+			case 'v': // vertex
+				for(sscanf(ptr += n, "%f%n", &v.y, &n); sscanf(ptr += n, "%f%n", &v.x, &n) == 1; )
+					{ vert = realloc(vert, ++NumVertices * sizeof(*vert)); vert[NumVertices-1] = v; }
+				break;
+			case 's': // sector
+				sectors = realloc(sectors, ++NumSectors * sizeof(*sectors));
+				struct sector* sect = &sectors[NumSectors-1];
+				int* num = NULL;
+				sscanf(ptr += n, "%f%f%n", &sect->floor,&sect->ceil, &n);
+				for(m=0; sscanf(ptr += n, "%32s%n", word, &n) == 1 && word[0] != '#'; )
+					{ num = realloc(num, ++m * sizeof(*num)); num[m-1] = word[0]=='x' ? -1 : atoi(word); }
+				sect->npoints   = m /= 2;
+				sect->neighbors = malloc( (m  ) * sizeof(*sect->neighbors) );
+				sect->vertex    = malloc( (m+1) * sizeof(*sect->vertex)    );
+				for(n=0; n<m; ++n) sect->neighbors[n] = num[m + n];
+				for(n=0; n<m; ++n) sect->vertex[n+1]  = vert[num[n]]; // TODO: Range checking
+				sect->vertex[0] = sect->vertex[m]; // Ensure the vertexes form a loop
+				free(num);
+				break;
+			case 'p':; // player
+				float angle;
+				sscanf(ptr += n, "%f %f %f %d", &v.x, &v.y, &angle,&n);
+				player = (struct player) { {v.x, v.y, 0}, {0,0,0}, angle,0,0,0, n }; // TODO: Range checking
+				player.where.z = sectors[player.sector].floor + EyeHeight;
+		}
+	}
     fclose(fp);
     free(vert);
 }
